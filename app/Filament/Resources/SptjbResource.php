bd9fc1d6be5d298b9b49dd4d873e0137810093f8
Muh. Ikhsan <133651055+ikhsan990@users.noreply.github.com>
2025-05-29 22:42:00 +0900

    sptjb add

--- app/Filament/Resources/SptjbResource.php
+++ app/Filament/Resources/SptjbResource.php
@@ -8,15 +8,19 @@ use Filament\Resources\Resource;
 use Filament\Tables\Table;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Filters\SelectFilter;
+use Barryvdh\DomPDF\Facade\Pdf;
+use Filament\Tables\Actions\Action;
+use Illuminate\Support\Facades\URL;
 
 class SptjbResource extends Resource
 {
     protected static ?string $model = Jadwal::class;
-
     protected static ?string $navigationLabel = 'SPTJB';
-    protected static ?string $navigationIcon = 'heroicon-o-collection';
-    protected static ?string $navigationGroup = 'Rekapitulasi';
-    protected static ?int $navigationSort = 2;
+    protected static ?string $navigationIcon = 'heroicon-o-clipboard-document-list';
+    protected static ?string $navigationGroup = '';
+    protected static ?int $navigationSort = 5;
+    protected static bool $shouldSkipAuthorization = true;
+
 
     public static function table(Table $table): Table
     {
@@ -24,7 +28,7 @@ class SptjbResource extends Resource
             ->columns([
                 TextColumn::make('nomor_spt')->label('Nomor SPT')->sortable()->searchable(),
                 TextColumn::make('tanggal_mulai')->label('Tanggal Mulai')->date()->sortable(),
-                TextColumn::make('tanggal_selesai')->label('Tanggal Selesai')->date()->sortable(),
+
                 TextColumn::make('kegiatan.nama_kegiatan')->label('Kegiatan')->sortable()->searchable(),
                 TextColumn::make('pegawai.nama')->label('Pegawai')->sortable()->searchable(),
                 TextColumn::make('tujuan')->label('Tujuan')->searchable(),
@@ -38,9 +42,21 @@ class SptjbResource extends Resource
                     ->label('Filter Pegawai')
                     ->relationship('pegawai', 'nama'),
             ])
+            ->actions([
+                //
+                ])
             ->defaultSort('tanggal_mulai', 'desc');
     }
 
+
+
+    public function exportPdf()
+{
+    $jadwals = \App\Models\Jadwal::with(['kegiatan', 'pegawai'])->get();
+
+    $pdf = Pdf::loadView('exports.sptjb', compact('jadwals'));
+    return $pdf->download('rekap_sptjb_' . now()->format('Ymd_His') . '.pdf');
+}
     public static function getPages(): array
     {
         return [
--- app/Filament/Resources/SptjbResource/Pages/ListSptjbs.php
+++ app/Filament/Resources/SptjbResource/Pages/ListSptjbs.php
@@ -2,10 +2,30 @@
 
 namespace App\Filament\Resources\SptjbResource\Pages;
 
+use Filament\Actions;
+use Illuminate\Support\Facades\URL;
 use App\Filament\Resources\SptjbResource;
 use Filament\Resources\Pages\ListRecords;
+use Filament\Pages\Actions\Action;
 
 class ListSptjbs extends ListRecords
 {
     protected static string $resource = SptjbResource::class;
+
+        protected function getHeaderActions(): array
+    {
+        return [
+            Actions\CreateAction::make('export_excel')
+                ->label('Export Excel')
+                ->icon('heroicon-m-magnifying-glass')
+                ->url(fn () => URL::signedRoute('filament.resources.sptjb.export-excel'))
+                ->openUrlInNewTab(),
+
+            Actions\CreateAction::make('export_pdf')
+                ->label('Export PDF')
+                ->icon('heroicon-m-magnifying-glass')
+                ->url(fn () => URL::signedRoute('filament.resources.sptjb.export-pdf'))
+                ->openUrlInNewTab(),
+        ];
+    }
 }
