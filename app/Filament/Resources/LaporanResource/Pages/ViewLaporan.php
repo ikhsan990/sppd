f0fef77b463aa95fed4f94e55392e4ee38a85b45
Muh. Ikhsan <133651055+ikhsan990@users.noreply.github.com>
2025-07-13 20:57:15 +0900

    Update ViewLaporan.php

--- app/Filament/Resources/LaporanResource/Pages/ViewLaporan.php
+++ app/Filament/Resources/LaporanResource/Pages/ViewLaporan.php
@@ -9,30 +9,5 @@ class ViewLaporan extends ViewRecord
 {
     protected static string $resource = LaporanResource::class;
 
-    //     protected function getViewData(): array
-    // {
-    //     $data = parent::getViewData();
 
-    //     // Ambil data foto yang mungkin berupa JSON array string
-    //     $foto = $this->record->foto;
-
-    //     // Jika foto adalah JSON array, decode dan buat URL yang benar
-    //     $fotoUrls = [];
-
-    //     if ($foto) {
-    //         $files = json_decode($foto, true);
-    //         if (is_array($files)) {
-    //             foreach ($files as $file) {
-    //                 $fotoUrls[] = asset('storage/foto/' . $file);
-    //             }
-    //         } else {
-    //             // Bila bukan array, misal string path langsung
-    //             $fotoUrls[] = asset('storage/foto/' . $foto);
-    //         }
-    //     }
-
-    //     $data['fotoUrls'] = $fotoUrls;
-
-    //     return $data;
-    // }
 }
